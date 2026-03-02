346693b1558391a987893cc95f15034238f8a875
Marcelo Hernandez <109430910+Pryxter@users.noreply.github.com>
2026-03-01 21:07:58 -0600

    Add files via upload

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://survex.app",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://survex.app/login",
+      lastModified: new Date().toISOString(),
+    },
+    ,
+    {
+      url: "https://survex.app/signup",
+      lastModified: new Date().toISOString(),
+    },
+  ];
+}
